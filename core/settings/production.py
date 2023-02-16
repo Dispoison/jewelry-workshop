5a1d9295c8f821ecce1f1921905f4a2cbab3c95b
Vitalii Maliarenko <malyarenko.vitalij.v@hneu.net>
2023-02-16 14:42:16 +0200

    Update production settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -6,6 +6,4 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["dispoison.pythonanywhere.com"]
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
