743d71f54a16877a95cc30c7f00e60eb1ee0e309
Vitalii Maliarenko <malyarenko.vitalij.v@hneu.net>
2023-02-16 14:00:54 +0200

    Update static settings

--- core/settings/development.py
+++ core/settings/development.py
@@ -1,3 +1,10 @@
+import os
+
+from core.settings import BASE_DIR
+
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
--- core/settings/production.py
+++ core/settings/production.py
@@ -1,3 +1,11 @@
+import os
+
+from core.settings import BASE_DIR
+
 DEBUG = False
 
 ALLOWED_HOSTS = ["dispoison.pythonanywhere.com"]
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
